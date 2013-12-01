544cbd6574f20f56eb0ff1dd46c6cac3dbbcd7b3
acmerfight <acmerfight@gmail.com>
2013-12-01 15:40:16 +0800

    add feature: about db

--- glegooder.py
+++ glegooder.py
@@ -1,3 +1,5 @@
+from contextlib import closing
+
 import pymongo
 from flask import Flask
 
@@ -12,8 +14,15 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-def create_mongo_db():
-    return pymongo.MongoClient()
+def get_db():
+    if not hasattr(g, "connection"):
+        g.connection = pymongo.MongoClient()
+    return g.connection
+
+@teardown_request
+def teardown_request(error):
+    if hasattr(g, "connection"):
+        g.connection.disconnect()
 
 
 if __name__ == "__main__":
